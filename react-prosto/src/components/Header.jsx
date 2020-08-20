d52ff0c5439289198ffe8c5c3cabd6950857564d
Сергей <41532402+Svift87@users.noreply.github.com>
2020-08-20 16:53:29 +0700

    add New project

---
+++ react-prosto/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src='https://cdnimg.rg.ru/img/content/174/48/56/DB2019AL01916_large_d_850.jpg' />
+        </header>
+    )
+}
+
+export default Header
---
+++ react-prosto/src/components/Profile.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div className="content__banner">
+                <img src="https://best-quote.ru/wp-content/uploads/2019/01/rossiya_harkov_park_vecher_lavochka_879_1600x900-e1549035496929.jpg" />
+            </div>
+            <div className="content__info">
+                <div className="content__info__avatar">
+                    <img src="https://static.mk.ru/upload/entities/2020/08/13/17/articles/detailPicture/9d/5d/76/5a/4991c61486bc262f5d90dc64fc67cec9.jpg" />
+                </div>
+                <div className="content__info__description">
+                    <h2 className="content__info__description--name">
+                        Sergey K.
+            			</h2>
+                    <p className="content__info__description--date">
+                        Date of Birth: 22 january
+						</p>
+                    <p className="content__info__description--city">
+                        Cyty: Novosibirsk
+            			</p>
+                    <p className="content__info__description--site">
+                        Web Site: https://novosibirsk.hh.ru/resume/08d915c2ff0579b6750039ed1f4c716e336372
+            			</p>
+                </div>
+            </div>
+            <div className="content__posts">
+                <h2 className="content__posts--name">
+                    My posts
+					</h2>
+                <div className="content__posts__container">
+                    <div className="content__posts__item">
+                        Posts 1
+						</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
