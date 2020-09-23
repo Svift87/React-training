a558fbab73e7d9245420f035c0fc85d7e7ddc3f1
Сергей <41532402+Svift87@users.noreply.github.com>
2020-09-23 18:55:32 +0700

    added redux

--- react-prosto/src/Redux/messageReducer.js
+++ react-prosto/src/Redux/messageReducer.js
@@ -1,7 +1,23 @@
 const ADD_MESSAGE = 'ADD-MESSAGE'
 const ADD_MESSAGE_TEXT = 'ADD-MESSAGE-TEXT'
 
-export const messageReducer = (state, action) => {
+let initialState = {
+    dialogsData: [
+        { id: 1, name: "Dima" },
+        { id: 2, name: "Nik" },
+        { id: 3, name: "Sonya" },
+        { id: 4, name: "Vitya" },
+    ],
+    messagesData: [
+        { id: 1, message: "Сообщения Dima" },
+        { id: 2, message: "Сообщения Nik" },
+        { id: 3, message: "Сообщения Sonya" },
+        { id: 4, message: "Сообщения Vitya" },
+    ],
+    newMessage: ''
+}
+
+export const messageReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_MESSAGE_TEXT:
             state.newMessage = action.text;   
--- react-prosto/src/Redux/profileReducer.js
+++ react-prosto/src/Redux/profileReducer.js
@@ -1,7 +1,24 @@
 const ADD_TEXT = 'ADD-TEXT'
 const ADD_POST = 'ADD-POST'
 
-const profileReducer = (state, action) => {
+let initialState = {
+    postData: [
+        { id: 1, message: "Hi, how are you?", like: 10 },
+        { id: 2, message: "It`s my first post", like: 15 },
+    ],
+    profileInfoData: {
+        id: 1,
+        banner: 'https://best-quote.ru/wp-content/uploads/2019/01/rossiya_harkov_park_vecher_lavochka_879_1600x900-e1549035496929.jpg',
+        avatar: 'https://static.mk.ru/upload/entities/2020/08/13/17/articles/detailPicture/9d/5d/76/5a/4991c61486bc262f5d90dc64fc67cec9.jpg',
+        name: 'Sergey K',
+        birthday: '22 juanvar',
+        city: 'Novosibirsk',
+        webSite: 'pokanet'
+    },
+    textInPost: ''
+}
+
+const profileReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_TEXT:
             state.textInPost = action.text;      
